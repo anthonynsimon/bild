1bf1a61a2e961de9e86b7a425c2201749225361f
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2016-08-01 23:38:19 +0200

    added brightness method

--- effects.go
+++ effects.go
@@ -17,3 +17,20 @@ func Invert(src image.Image) *image.NRGBA {
 
 	return img
 }
+
+// Brightness returns a copy of the image with the adjusted brightness
+func Brightness(src image.Image, value int) *image.NRGBA {
+	img := cloneAsNRGBA(src)
+
+	fn := func(c color.NRGBA) color.NRGBA {
+		return color.NRGBA{
+			uint8(clamp(int(c.R)+value, 0, 255)),
+			uint8(clamp(int(c.G)+value, 0, 255)),
+			uint8(clamp(int(c.B)+value, 0, 255)),
+			c.A}
+	}
+
+	apply(img, fn)
+
+	return img
+}
