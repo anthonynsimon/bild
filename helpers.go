6d0c59967c6b9c36843d45a731ad01d9ef5d720f
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2016-08-03 01:06:02 +0200

    switching to RGBA

--- helpers.go
+++ helpers.go
@@ -3,38 +3,34 @@ package bild
 import (
 	"image"
 	"image/color"
+	"image/draw"
 	"math"
 )
 
-// returns an NRGBA copy of the image
-func cloneAsNRGBA(src image.Image) *image.NRGBA {
+// CloneAsRGBA returns an RGBA copy of the image
+func CloneAsRGBA(src image.Image) *image.RGBA {
 	bounds := src.Bounds()
-	w, h := bounds.Max.X, bounds.Max.Y
-	result := image.NewNRGBA(bounds)
-
-	parallelize(h, func(start, end int) {
-		for x := 0; x < w; x++ {
-			for y := start; y < end; y++ {
-				result.Set(x, y, src.At(x, y))
-			}
-		}
-	})
-
-	return result
+	img := image.NewRGBA(bounds)
+	draw.Draw(img, bounds, src, bounds.Min, draw.Src)
+	return img
 }
 
-// applies a color function to each pixel on an image
-func apply(img *image.NRGBA, fn func(color.NRGBA) color.NRGBA) {
+// Apply returns a copy of the image after applying a color function
+// to each pixel on an image
+func apply(img image.Image, fn func(color.RGBA) color.RGBA) *image.RGBA {
 	bounds := img.Bounds()
+	dst := CloneAsRGBA(img)
 	w, h := bounds.Max.X, bounds.Max.Y
 
 	parallelize(h, func(start, end int) {
 		for x := 0; x < w; x++ {
 			for y := start; y < end; y++ {
-				img.Set(x, y, fn(img.NRGBAAt(x, y)))
+				dst.Set(x, y, fn(dst.RGBAAt(x, y)))
 			}
 		}
 	})
+
+	return dst
 }
 
 func clamp(value, min, max float64) float64 {
