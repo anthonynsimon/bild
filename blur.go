29242232e2df1c1cebc5fe4c7534f18744fedcc6
Anthony N. Simon <anthonynsimon@gmail.com>
2016-08-02 14:40:20 +0200

    simplified blur functions

--- blur.go
+++ blur.go
@@ -1,32 +1,36 @@
 package bild
 
-import "image"
+import (
+	"image"
+	"math"
+)
 
 // Box Blur returns a blurred (average) version of the image
-func Box(src image.Image) *image.NRGBA {
-	k := Kernel{[3][3]int32{
-		{1, 1, 1},
-		{1, 1, 1},
-		{1, 1, 1},
-	}}
+func Box(src image.Image, size int) *image.NRGBA {
+	k := NewKernel(size)
+
+	for x := 0; x < size; x++ {
+		for y := 0; y < size; y++ {
+			k.Matrix[x][y] = 1
+		}
+	}
 
 	img := cloneAsNRGBA(src)
-	return convolute(img, &k)
+	return convolute(img, k)
 }
 
 // Gaussian Blur returns a blurred version of the image using
 // an approximation to the Gaussian function
-func Gaussian(src image.Image, iterations int) *image.NRGBA {
-	k := Kernel{[3][3]int32{
-		{1, 2, 1},
-		{2, 4, 2},
-		{1, 2, 1},
-	}}
+func Gaussian(src image.Image, radius float64) *image.NRGBA {
+	size := int(math.Ceil(radius) * 2)
+	k := NewKernel(size + 1)
 
-	result := cloneAsNRGBA(src)
-	for i := 0; i < iterations; i++ {
-		result = convolute(result, &k)
+	for x := 0; x <= size; x++ {
+		for y := 0; y <= size; y++ {
+			k.Matrix[x][y] = gaussianFunc(float64(x)-radius, float64(y)-radius, radius)
+		}
 	}
 
-	return result
+	img := cloneAsNRGBA(src)
+	return convolute(img, k)
 }
