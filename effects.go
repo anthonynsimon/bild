87bf52e929340e74e9e432b64733828ae154d95c
Anthony N. Simon <anthonynsimon@gmail.com>
2016-08-04 10:45:25 +0200

    added median effect and helpers

--- effects.go
+++ effects.go
@@ -57,3 +57,54 @@ func Emboss(src image.Image) *image.RGBA {
 
 	return Convolute(src, &k, 128)
 }
+
+// Median returns a new image in which each pixel is the mean of it's neighbors
+func Median(img image.Image, size int) *image.RGBA {
+	bounds := img.Bounds()
+	src := CloneAsRGBA(img)
+
+	if size < 0 {
+		return src
+	}
+
+	dst := image.NewRGBA(bounds)
+
+	w, h := bounds.Max.X, bounds.Max.Y
+	neighborsCount := size * size
+
+	parallelize(h, func(start, end int) {
+		for x := 0; x < w; x++ {
+			for y := start; y < end; y++ {
+
+				neighbors := make([]color.RGBA, neighborsCount)
+				i := 0
+				for kx := 0; kx < size; kx++ {
+					for ky := 0; ky < size; ky++ {
+						ix := (x - size/2 + kx + w) % (w)
+						iy := (y - size/2 + ky + h) % h
+
+						ipos := iy*dst.Stride + ix*4
+						neighbors[i] = color.RGBA{
+							R: src.Pix[ipos+0],
+							G: src.Pix[ipos+1],
+							B: src.Pix[ipos+2],
+							A: src.Pix[ipos+3],
+						}
+						i++
+					}
+				}
+
+				quicksortRGBA(neighbors, 0, neighborsCount-1)
+				median := neighbors[neighborsCount/2]
+
+				pos := y*dst.Stride + x*4
+				dst.Pix[pos+0] = median.R
+				dst.Pix[pos+1] = median.G
+				dst.Pix[pos+2] = median.B
+				dst.Pix[pos+3] = median.A
+			}
+		}
+	})
+
+	return dst
+}
--- helpers.go
+++ helpers.go
@@ -46,3 +46,34 @@ func clamp(value, min, max float64) float64 {
 func gaussianFunc(x, y, sigma float64) float64 {
 	return math.Exp(-(x*x/sigma + y*y/sigma))
 }
+
+func quicksortRGBA(data []color.RGBA, min, max int) {
+	if min > max {
+		return
+	}
+	p := partitionRGBASlice(data, min, max)
+	quicksortRGBA(data, min, p-1)
+	quicksortRGBA(data, p+1, max)
+}
+
+func partitionRGBASlice(data []color.RGBA, min, max int) int {
+	pivot := data[max]
+	i := min
+	for j := min; j < max; j++ {
+		if rank(data[j]) <= rank(pivot) {
+			temp := data[i]
+			data[i] = data[j]
+			data[j] = temp
+			i++
+		}
+	}
+	temp := data[i]
+	data[i] = data[max]
+	data[max] = temp
+	return i
+}
+
+// Rank a color based on a color perception heuristic
+func rank(c color.RGBA) float64 {
+	return float64(c.R)*0.3 + float64(c.G)*0.6 + float64(c.B)*0.1
+}
