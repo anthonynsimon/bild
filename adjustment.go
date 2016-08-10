5811374d4b77e39ed5d3663e384a8118cb66553f
Anthony N. Simon <anthonynsimon@gmail.com>
2016-08-10 11:39:10 +0200

    improved brightness performance by chaching values

--- adjustment.go
+++ adjustment.go
@@ -9,17 +9,14 @@ import (
 // Brightness returns a copy of the image with the adjusted brightness.
 // Change is the normalized amount of change to be applied (range -1.0 to 1.0).
 func Brightness(src image.Image, change float64) *image.RGBA {
+	cache := make([]uint8, 256)
+
+	for i := 0; i < 256; i++ {
+		cache[i] = uint8(clampFloat64(math.Ceil(float64(i)*(1+change)), 0, 255))
+	}
+
 	fn := func(c color.RGBA) color.RGBA {
-
-		changeR := 1 + change
-		changeG := 1 + change
-		changeB := 1 + change
-
-		return color.RGBA{
-			uint8(clampFloat64(math.Ceil(float64(c.R)*changeR), 0, 255)),
-			uint8(clampFloat64(math.Ceil(float64(c.G)*changeG), 0, 255)),
-			uint8(clampFloat64(math.Ceil(float64(c.B)*changeB), 0, 255)),
-			c.A}
+		return color.RGBA{cache[c.R], cache[c.G], cache[c.B], c.A}
 	}
 
 	img := apply(src, fn)
