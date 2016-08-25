69fe5cc86c30cb078e3412074a0ee310ea136b0e
Anthony N. Simon <anthonynsimon@gmail.com>
2016-08-25 16:00:46 +0200

    Remove untested equalize

--- adjustment.go
+++ adjustment.go
@@ -61,29 +61,3 @@ func Contrast(src image.Image, change float64) *image.RGBA {
 
 	return img
 }
-
-func Equalize(img image.Image) *image.RGBA {
-	src := CloneAsRGBA(img)
-	srcW, srcH := src.Bounds().Dx(), src.Bounds().Dy()
-	dst := image.NewRGBA(src.Bounds())
-
-	hist := NewRGBAHistogram(img)
-	hist = hist.Cumulative()
-
-	fn := func(h Histogram, min, value int) int {
-		return (((h.Bins[value] - min) << 16 / ((srcW * srcH) - min)) * 255) >> 16
-	}
-
-	rMin, gMin, bMin := hist.R.Min(), hist.G.Min(), hist.B.Min()
-
-	for x := 0; x < srcW; x++ {
-		for y := 0; y < srcH; y++ {
-			pos := y*src.Stride + x*4
-			dst.Pix[pos+0] = uint8(fn(hist.R, rMin, int(src.Pix[pos+0])))
-			dst.Pix[pos+1] = uint8(fn(hist.G, gMin, int(src.Pix[pos+1])))
-			dst.Pix[pos+2] = uint8(fn(hist.B, bMin, int(src.Pix[pos+2])))
-			dst.Pix[pos+3] = src.Pix[pos+3]
-		}
-	}
-	return dst
-}
