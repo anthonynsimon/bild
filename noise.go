26dc738ba13184ddf158575d323cb978c77b318a
Anthony N. Simon <anthonynsimon@gmail.com>
2016-08-26 11:24:40 +0200

    Add noise types

--- noise.go
+++ noise.go
@@ -5,19 +5,60 @@ import (
 	"math/rand"
 )
 
-func Noise(width, height int) *image.Gray {
-	dst := image.NewGray(image.Rect(0, 0, width, height))
+type NoiseType func() uint8
+
+var (
+	Uniform       NoiseType
+	Binary        NoiseType
+	GaussianNoise NoiseType
+)
+
+func init() {
+	Uniform = func() uint8 {
+		return uint8(rand.Intn(256))
+	}
+	Binary = func() uint8 {
+		return 0xFF * uint8(rand.Intn(2))
+	}
+	GaussianNoise = func() uint8 {
+		return uint8(rand.NormFloat64()*32.0 + 128.0)
+	}
+}
+
+type NoiseOptions struct {
+	Fn      NoiseType
+	Colored bool
+}
+
+func Noise(width, height int, o *NoiseOptions) *image.RGBA {
+	dst := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	noiseFn := Uniform
+	colored := false
+	if o != nil {
+		if o.Fn != nil {
+			noiseFn = o.Fn
+		}
+		colored = o.Colored
+	}
 
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
-			dst.Pix[y*dst.Stride+x] = uint8(rand.Intn(255))
+			pos := y*dst.Stride + x*4
+			if colored {
+				dst.Pix[pos+0] = noiseFn()
+				dst.Pix[pos+1] = noiseFn()
+				dst.Pix[pos+2] = noiseFn()
+				dst.Pix[pos+3] = 0xFF
+			} else {
+				v := noiseFn()
+				dst.Pix[pos+0] = v
+				dst.Pix[pos+1] = v
+				dst.Pix[pos+2] = v
+				dst.Pix[pos+3] = 0xFF
+			}
 		}
 	}
 
 	return dst
-}
-
-func SmoothNoise(width, height int) *image.Gray {
-	// TODO
-	return nil
 }
