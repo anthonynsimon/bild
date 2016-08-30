231c6fe50782455837f60f670d8dcb7a62f9a649
Anthony N. Simon <anthonynsimon@gmail.com>
2016-08-30 14:06:21 +0200

    Simplify shear implementation

--- transform/skew.go
+++ transform/skew.go
@@ -7,22 +7,38 @@ import (
 	"github.com/anthonynsimon/bild/parallel"
 )
 
-func Shear(img image.Image, amountX, amountY float64) *image.RGBA {
+func Shear(img image.Image, horizontal, vertical float64) *image.RGBA {
 	src := clone.AsRGBA(img)
 	srcW, srcH := src.Bounds().Dx(), src.Bounds().Dy()
 
-	// Default pivot position is center of image
-	pivotX, pivotY := float64(srcW)*amountX/2, float64(srcH)*amountY/2
+	// Supersample, currently hard set to 2x
+	srcW, srcH = srcW*2, srcH*2
+	src = Resize(src, srcW, srcH, NearestNeighbor)
 
 	dstW, dstH := srcW, srcH
 	dst := image.NewRGBA(image.Rect(0, 0, dstW, dstH))
 
-	parallel.Line(srcH, func(start, end int) {
+	pivotX, pivotY := float64(dstW)/2, float64(dstH)/2
 
+	// // Calculate pixels as percent of pivot shift
+	// // Scale by 2 for supersampling
+	horizontal = horizontal * 2 / pivotX
+	vertical = vertical * 2 / pivotY
+
+	parallel.Line(dstH, func(start, end int) {
 		for y := start; y < end; y++ {
 			for x := 0; x < dstW; x++ {
-				ix := int(float64(x) + float64(y)*amountX - pivotX)
-				iy := int(float64(y) + float64(x)*amountY - pivotY)
+				// Move positions to revolve around pivot
+				ix := x - int(pivotX)
+				iy := y - int(pivotY)
+
+				// Apply linear transformation
+				ix = ix + int(float64(iy)*horizontal)
+				iy = iy + int(float64(ix)*vertical)
+
+				// Move positions back to image coordinates
+				ix += int(pivotX)
+				iy += int(pivotY)
 
 				if ix < 0 || ix >= srcW || iy < 0 || iy >= srcH {
 					continue
@@ -38,6 +54,9 @@ func Shear(img image.Image, amountX, amountY float64) *image.RGBA {
 			}
 		}
 	})
+
+	// Downsample to original bounds as part of the Supersampling
+	dst = Resize(dst, dstW/2, dstH/2, Linear)
 
 	return dst
 }
