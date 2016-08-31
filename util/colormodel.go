938f4fc67198d32264be4eb92e27b80865e3e130
Anthony N. Simon <anthonynsimon@gmail.com>
2016-08-31 15:57:43 +0200

    Add HSL to RGB

--- util/colormodel.go
+++ util/colormodel.go
@@ -7,29 +7,26 @@ import (
 	"github.com/anthonynsimon/bild/math/f64"
 )
 
-func RGBToHSL(c color.RGBA) (h, s, l float64) {
+func RGBToHSL(c color.RGBA) (float64, float64, float64) {
 	r, g, b := float64(c.R)/255, float64(c.G)/255, float64(c.B)/255
-
 	max := math.Max(r, math.Max(g, b))
 	min := math.Min(r, math.Min(g, b))
 	delta := max - min
 
+	var h, s, l float64
 	l = (max + min) / 2
 
-	// Avoid division by zero
-	if max == 0 {
-		h = 0
-		s = 0
-		return
+	// Achromatic
+	if delta <= 0 {
+		return h, s, l
 	}
 
-	s = delta / (1 - math.Abs(2*l-1))
-
-	// // Achromatic
-	// if max == min {
-	// 	h = 0
-	// 	return
-	// }
+	// Should it be smaller than or equals instead?
+	if l < 0.5 {
+		s = delta / (max + min)
+	} else {
+		s = delta / (2 - max - min)
+	}
 
 	if r >= max {
 		h = (g - b) / delta
@@ -44,7 +41,57 @@ func RGBToHSL(c color.RGBA) (h, s, l float64) {
 		h += 360
 	}
 
-	return
+	return h, s, l
+}
+
+func HSLToRGB(h, s, l float64) color.RGBA {
+
+	var r, g, b float64
+	if s == 0 {
+		r = l
+		g = l
+		b = l
+	} else {
+		var temp0, temp1 float64
+		if l < 0.5 {
+			temp0 = l * (1 + s)
+		} else {
+			temp0 = (l + s) - (s * l)
+		}
+		temp1 = 2*l - temp0
+
+		h /= 360
+
+		hueFn := func(v float64) float64 {
+			if v < 0 {
+				v++
+			} else if v > 1 {
+				v--
+			}
+
+			if v < 1.0/6.0 {
+				return temp1 + (temp0-temp1)*6*v
+			}
+			if v < 1.0/2.0 {
+				return temp0
+			}
+			if v < 2.0/3.0 {
+				return temp1 + (temp0-temp1)*(2.0/3.0-v)*6
+			}
+			return temp1
+		}
+
+		r = hueFn(h + 1.0/3.0)
+		g = hueFn(h)
+		b = hueFn(h - 1.0/3.0)
+
+	}
+
+	outR := uint8(f64.Clamp(r*255+0.5, 0, 255))
+	outG := uint8(f64.Clamp(g*255+0.5, 0, 255))
+	outB := uint8(f64.Clamp(b*255+0.5, 0, 255))
+
+	return color.RGBA{outR, outG, outB, 0xFF}
 }
 
 func RGBToHSV(c color.RGBA) (h, s, v float64) {
