765ba5a0aa40c5e20026e1b47ebd44faf4b14819
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2016-09-18 21:09:14 +0200

    Add exported methods docs

--- util/stack.go
+++ util/stack.go
@@ -1,6 +1,6 @@
 package util
 
-//A utility stack implementation for arbitrary data types with Push(), Pop() and Len() functions
+// Stack implementation for arbitrary data types with Push(), Pop() and Len() functions
 type Stack struct {
 	top  *stackElement
 	size int
@@ -11,15 +11,18 @@ type stackElement struct {
 	next  *stackElement
 }
 
+// Len returns the size of stack
 func (s *Stack) Len() int {
 	return s.size
 }
 
+// Push a new value onto the stack
 func (s *Stack) Push(value interface{}) {
 	s.top = &stackElement{value, s.top}
 	s.size++
 }
 
+// Pop the most recently pushed value from the stack
 func (s *Stack) Pop() interface{} {
 	if s.size > 0 {
 		value := s.top.value
