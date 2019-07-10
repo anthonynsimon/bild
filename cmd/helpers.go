fbe0162b358948f1c010142a0219400fb4aee8e7
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2019-07-10 22:43:56 +0200

    Resolve encoding from filename

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -3,9 +3,31 @@ package cmd
 import (
 	"image"
 	"log"
+	"strings"
 
 	"github.com/anthonynsimon/bild/imgio"
 )
+
+var jpgExtensions = []string{".jpg", ".jpeg"}
+var pngExtensions = []string{".png"}
+
+func resolveEncoder(outputfile string, defaultEncoding imgio.Encoder) imgio.Encoder {
+	lower := strings.ToLower(outputfile)
+
+	for _, ext := range jpgExtensions {
+		if strings.HasSuffix(lower, ext) {
+			return imgio.JPEGEncoder(100)
+		}
+	}
+
+	for _, ext := range pngExtensions {
+		if strings.HasSuffix(lower, ext) {
+			return imgio.PNGEncoder()
+		}
+	}
+
+	return defaultEncoding
+}
 
 func apply(fin, fout string, process func(image.Image) (image.Image, error)) {
 	in, err := imgio.Open(fin)
@@ -18,7 +40,8 @@ func apply(fin, fout string, process func(image.Image) (image.Image, error)) {
 		log.Fatal(err)
 	}
 
-	err = imgio.Save(fout, result, imgio.PNGEncoder())
+	encoder := resolveEncoder(fout, imgio.PNGEncoder())
+	err = imgio.Save(fout, result, encoder)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,7 +63,8 @@ func apply2(fin1, fin2, fout string, process func(image.Image, image.Image) (ima
 		log.Fatal(err)
 	}
 
-	err = imgio.Save(fout, result, imgio.PNGEncoder())
+	encoder := resolveEncoder(fout, imgio.PNGEncoder())
+	err = imgio.Save(fout, result, encoder)
 	if err != nil {
 		log.Fatal(err)
 	}
